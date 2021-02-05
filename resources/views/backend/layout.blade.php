9cb4647cd403e104832d857e6eff06ecdf739119
MHovi2 <mehedi174629@gmail.com>
2021-02-05 22:03:37 +0600

    footer

--- resources/views/backend/layout.blade.php
+++ resources/views/backend/layout.blade.php
@@ -389,7 +389,7 @@
 
     <footer class="main-footer">
       <div class="footer-left">
-        Copyright &copy; 2019 <div class="bullet"></div> Design By <a href="#">Redstar</a>
+        Copyright by&copy; 2019 <div class="bullet"></div> Design By <a href="#">Redstar</a>
       </div>
       <div class="footer-right">
       </div>
