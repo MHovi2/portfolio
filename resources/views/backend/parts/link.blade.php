e2ea370ebceebeaf0f351e137af7f7cf48dd090c
MHovi2 <mehedi174629@gmail.com>
2021-02-05 21:39:24 +0600

    Project Up

---
+++ resources/views/backend/parts/link.blade.php
@@ -0,0 +1,8 @@
+<!-- General CSS Files -->
+<link rel="stylesheet" href="assets/backend/css/app.min.css">
+<!-- Template CSS -->
+<link rel="stylesheet" href="assets/backend/css/style.css">
+<link rel="stylesheet" href="assets/backend/css/components.css">
+<!-- Custom style CSS -->
+<link rel="stylesheet" href="assets/backend/css/custom.css">
+<link rel='shortcut icon' type='image/x-icon' href='assets/backend/img/favicon.ico' />
---
+++ resources/views/backend/parts/script.blade.php
@@ -0,0 +1,10 @@
+ <!-- General JS Scripts -->
+ <script src="assets/backend/js/app.min.js"></script>
+ <!-- JS Libraies -->
+ <script src="assets/backend/bundles/apexcharts/apexcharts.min.js"></script>
+ <!-- Page Specific JS File -->
+ <script src="assets/backend/js/page/index.js"></script>
+ <!-- Template JS File -->
+ <script src="assets/backend/js/scripts.js"></script>
+ <!-- Custom JS File -->
+ <script src="assets/backend/js/custom.js"></script>
