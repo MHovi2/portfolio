916037d007b5fa1074fdaa5e507fb1d82b55148f
MHovi2 <mehedi174629@gmail.com>
2021-07-12 11:40:11 +0600

    Skills Page Update

--- resources/views/backend/skills.blade.php
+++ resources/views/backend/skills.blade.php
@@ -6,7 +6,7 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header">
-                            <h4>INFOS</h4>
+                            <h4>SKILLS</h4>
                         </div>
 
                        <form method="POST" action="{{route('skills')}}">
@@ -20,58 +20,74 @@
 
                             <div class="form-group row mb-4 mx-4">
                                 <div class="col-md-6">
-                                    <label>First Name</label>
-                                    <input type="range" class="form-control">
+                                    <label>HTML</label>
+
+                                    <center>
+                                        <span id="html_show"></span>
+                                    </center>
+                                    <input id="html" name="html" type="range" class="form-control">
                                 </div>
                                 <div class="col-md-6">
-                                    <label>Last Name</label>
-                                    <input type="range" class="form-control">
+                                    <label>CSS</label>
+
+                                    <center>
+                                        <span id="css_show"></span>
+                                    </center>
+                                    <input id="css" name="css" type="range" class="form-control">
                                 </div>
                             </div>
                             <div class="form-group row mb-4 mx-4">
                                 <div class="col-md-6">
-                                    <label>Age</label>
+                                    <label>JavaScript</label>
 
                                     <center>
-                                        <span id="age_show"></span>
+                                        <span id="js_show"></span>
                                     </center>
-                                    <input id="age" name="age" type="range" class="form-control">
+                                    <input id="js" name="js" type="range" class="form-control">
                                 </div>
                                 <div class="col-md-6">
-                                    <label>Address</label>
+                                    <label>PHP</label>
 
                                     <center>
-                                        <span id="address_show"></span>
+                                        <span id="php_show"></span>
                                     </center>
-                                    <input id="address" type="range" class="form-control" id="Address">
+                                    <input id="php" name="php" type="range" class="form-control">
                                 </div>
                             </div>
                             <div class="form-group row mb-4 mx-4">
                                 <div class="col-md-6">
-                                    <label>Phone</label>
+                                    <label>Laravel</label>
 
                                     <center>
-                                        <span id="address_show"></span>
+                                        <span id="laravel_show"></span>
                                     </center>
-                                    <input type="range" class="form-control">
+                                    <input id="laravel" name="laravel" type="range" class="form-control">
                                 </div>
                                 <div class="col-md-6">
-                                    <label>Email</label>
-                                    <input type="range" class="form-control">
+                                    <label>JQuery</label>
+
+                                    <center>
+                                        <span id="jquery1_show"></span>
+                                    </center>
+                                    <input id="jquery1" name="jquery1" type="range" class="form-control">
                                 </div>
                             </div>
                             <div class="form-group row mb-4 mx-4">
                                 <div class="col-md-6">
-                                    <label>Nationality</label>
+                                    <label>Word Press</label>
 
                                     <center>
-                                        <span id="address_show"></span>
+                                        <span id="wp_show"></span>
                                     </center>
-                                    <input type="range" class="form-control">
+                                    <input id="wp" name="wp" type="range" class="form-control">
                                 </div>
                                 <div class="col-md-6">
-                                    <label>Skype</label>
-                                    <input type="range" class="form-control">
+                                    <label>Bootstrap</label>
+
+                                     <center>
+                                        <span id="bstrap_show"></span>
+                                    </center>
+                                    <input id="bstrap" name="bstrap" type="range" class="form-control">
                                 </div>
                             </div>
                             <div class="form-group row mb-4 mx-4">
